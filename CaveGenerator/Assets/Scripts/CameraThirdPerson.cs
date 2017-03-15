f4995662ba668e2fdb768d276f92c16b4f8f393d
roamik <roam.ivaniv@gmail.com>
2017-03-16 01:17:36 +0200

    playerControllRemastered
    new controll

---
+++ CaveGenerator/Assets/Scripts/CameraThirdPerson.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraThirdPerson : MonoBehaviour
+{
+    public bool lockCursor;
+
+    public float mouseSensitivity = 10;
+    public float distFromTarget = 6;
+
+    public float zoomSpeed = 3f;
+
+    public float zoomMin = 5f;
+    public float zoomMax = 12f;
+
+    public Transform target;
+    
+    public Vector2 pitchMinMax = new Vector2(0, 85);
+
+    public float rotationSmoothTime = 0.12f;
+    Vector3 rotationSmoothVelocity;
+    Vector3 currentRotation;
+
+    float yaw;
+    float pitch;
+
+    void Start()
+    {
+        if(lockCursor)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+
+	void LateUpdate ()
+    {
+        distFromTarget -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+
+        if (distFromTarget <= zoomMin)
+        {
+            distFromTarget = zoomMin;
+        }
+
+        if (distFromTarget >= zoomMax)
+        {
+            distFromTarget = zoomMax;
+        }
+
+        yaw += Input.GetAxis("Mouse X") * mouseSensitivity;
+        pitch -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+        pitch = Mathf.Clamp(pitch, pitchMinMax.x, pitchMinMax.y);
+
+        currentRotation = Vector3.SmoothDamp(currentRotation, new Vector3(pitch, yaw), ref rotationSmoothVelocity, rotationSmoothTime);
+        transform.eulerAngles = currentRotation;
+
+        transform.position = target.position - transform.forward * distFromTarget;
+	}
+}
---
+++ CaveGenerator/Assets/Scripts/PlayerContoller.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerContoller : MonoBehaviour
+{
+
+    public float walkSpeed= 2;
+    public float runSpeed = 6;
+    public float gravity = -12;
+    public float jumpHeight = 2;
+    [Range(0,2)]
+    public float airControllPercent;
+
+    public float turnSmoothTime = 0.2f;
+    float turnSmoothVelocity;
+
+    public float speedSmoothTime = 0.1f;
+    float speedSmoothVelocity;
+    float currentSpeed;
+    float velocityY;
+
+    public Transform camTransform;
+
+    Animator animator;
+    Transform cameraT;
+
+    CharacterController controller;
+
+	void Start () {
+        animator = GetComponent<Animator>();
+        cameraT = Camera.main.transform;
+        controller = GetComponent<CharacterController>();
+	}
+	
+	void Update ()
+    {
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        Vector2 inputDir = input.normalized;
+        bool running = Input.GetKey(KeyCode.LeftShift);
+        Movement(inputDir, running);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+
+
+        float animationSpeedPercent = ((running) ? currentSpeed / runSpeed : currentSpeed / walkSpeed * 0.5f);
+        animator.SetFloat("Speed", animationSpeedPercent, speedSmoothTime, Time.deltaTime);
+    }
+
+    void Movement(Vector2 inputDir, bool running)
+    {
+
+        if (inputDir != Vector2.zero)
+        {
+            float targetRotation = Mathf.Atan2(inputDir.x, inputDir.y) * Mathf.Rad2Deg + cameraT.eulerAngles.y;
+            transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, GetModifiedSmoothTime(turnSmoothTime));
+        }
+
+        
+        float targetSpeed = ((running) ? runSpeed : walkSpeed) * inputDir.magnitude;
+        currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedSmoothVelocity, GetModifiedSmoothTime(speedSmoothTime));
+
+        velocityY += Time.deltaTime * gravity;
+
+        Vector3 velocity = transform.forward * currentSpeed + Vector3.up * velocityY;
+
+        controller.Move(velocity * Time.deltaTime);
+        currentSpeed = new Vector2(controller.velocity.x, controller.velocity.z).magnitude;
+
+        if(controller.isGrounded)
+        {
+            velocityY = 0;
+        }
+
+    }
+
+    void Jump()
+    {
+        if (controller.isGrounded)
+        {
+            float jumpVelocity = Mathf.Sqrt(-2 * gravity * jumpHeight);
+            velocityY = jumpVelocity;
+        }
+    }
+
+    public float GetModifiedSmoothTime (float smoothTime)
+    {
+        if(controller.isGrounded)
+        {
+            return smoothTime;
+        }
+
+        if(airControllPercent == 0)
+        {
+            return float.MaxValue;
+        }
+
+        return smoothTime / airControllPercent;
+    }
+}
